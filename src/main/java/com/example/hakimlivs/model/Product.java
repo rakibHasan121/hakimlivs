9e21faf43b92a7002c99694e20d8c52f6b7d7a96
Rakib <rakib121@gmail.com>
2021-04-15 15:15:35 +0200

    generationtype table

--- src/main/java/com/example/hakimlivs/model/Product.java
+++ src/main/java/com/example/hakimlivs/model/Product.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 @Table(name = "HAKIM_PRODUCTS")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     protected int id;
     protected String title;
     @Column(name="Description", length=1500, nullable=false)
