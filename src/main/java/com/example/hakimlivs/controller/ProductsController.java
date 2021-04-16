cfdc377bbedfd9e43859899eb3db897d79d27f7a
Rakib <rakib121@gmail.com>
2021-04-16 17:35:08 +0200

    added crossorigin

--- src/main/java/com/example/hakimlivs/controller/ProductsController.java
+++ src/main/java/com/example/hakimlivs/controller/ProductsController.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"https://hakims-livs.herokuapp.com/products"}, methods = {RequestMethod.GET, RequestMethod.POST})
+@CrossOrigin(origins = {"http://127.0.0.1:5500"})
 public class ProductsController {
     private final ProductService productService;
 
